b14c2c441283dd64f8b1336413e3a4089de7f1bb
Preeti Patil <patil.preeti9@gmail.com>
2017-06-03 14:33:26 -0700

    Add files via upload

--- src/Removeduplicates.java
+++ src/Removeduplicates.java
@@ -3,31 +3,21 @@ import java.util.Arrays;
 public class Removeduplicates {
 	public void removeDups(int[] inputarray){
 		int length = inputarray.length;
-		System.out.println(length);
+		System.out.println("Length :"+length);
 		Arrays.sort(inputarray);
-		int[] array2 = {};
+		int[] array2 = new int[length];
 		int j = 0;
+		
 		for(int i = 0 ; i<length-1 ; i++){
-			System.out.println(inputarray[i+1]);
-			if(inputarray[i] == inputarray[i+1]){
-				System.out.println("Enterd here");
-				array2[j] = inputarray[i];
-				
-				j++;
-			}
-			else{
-				System.out.println("Enterd else");
-				array2[j] = inputarray[i];
-				i++;
-			}
-			array2[j++] = inputarray[length-1];
+			if(inputarray[i] != inputarray[i+1])
+				array2[j++] = inputarray[i];
 		}
-		
-		for(int s =0 ; s<=length-1 ; s++){
+		array2[j++] = inputarray[length-1];
+
+		for(int s=0 ; s<j ; s++){
 			inputarray[s] = array2[s];
-			System.out.println(inputarray[s]);
+			System.out.print(inputarray[s]+" ");
 		}
-		
 	}
 }
 
