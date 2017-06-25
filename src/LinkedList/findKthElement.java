e7b108a7d69f98d6311dfda88ae9e1f992c59330
Preeti1302 <patil.preeti9@gmail.com>
2017-06-24 17:50:26 -0700

    LinkedList: find kth element from last

--- src/LinkedList/findKthElement.java
+++ src/LinkedList/findKthElement.java
@@ -1,5 +1,24 @@
 package LinkedList;
-
+import java.util.LinkedList;
 public class findKthElement {
+
+	public static void findElement(LinkedList<Integer> list , int n){
+		int size = list.size();
+		int findIndex = size - n;
+		System.out.println(list.get(findIndex));
+	}
+		
+	public static void main(String[] args){
+
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		list.add(10); list.add(20);
+		list.add(30); list.add(40);
+		list.add(50); list.add(60);
+		System.out.println(list);
+		System.out.println(list.size());
+		int n = 3;
+		findElement(list , n);
+		
+	}
 
 }
